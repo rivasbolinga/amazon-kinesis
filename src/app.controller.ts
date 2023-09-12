196b7cdab4c41d3d13154a435c0fe9ac01ca6d9c
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-09-12 15:25:25 +0200

    Add webcam

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  root(@Res() res: Response) {
+    return res.render('index');
   }
 }
