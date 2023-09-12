c3fe569279bec4b29a2d46158b06449cfc923e41
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-09-12 15:36:07 +0200

    Create template with video webcam component

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { KinesisVideo } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  private kinesisVideo: KinesisVideo;
+  constructor(private readonly configService: ConfigService) {
+    this.kinesisVideo = new KinesisVideo({
+      accessKeyId: 'ACCESS_KEY_ID_GOES_HERE',
+      secretAccessKey: 'SECRET_ACCESS_KEY_GOES_HERE',
+      region: 'us-west-2',
+    });
+  }
 }
