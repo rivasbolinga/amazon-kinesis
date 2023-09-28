f0d867bc750ce391be4351c1ca3aeaaae309b31b
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-09-28 12:42:14 +0200

    Add webcam

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { KinesisVideo } from 'aws-sdk';
-import { ConfigService } from '@nestjs/config';
+import AWS from 'aws-sdk';
+import { SignalingClient } from 'amazon-kinesis-video-streams-webrtc';
 
 @Injectable()
 export class AppService {
-  private kinesisVideo: KinesisVideo;
-  constructor(private readonly configService: ConfigService) {
-    this.kinesisVideo = new KinesisVideo({
-      accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
-      region: 'us-west-2',
-    });
-  }
+  constructor() {
+    // DescribeSignalingChannel API can also be used to get the ARN from a channel name.
+  //   const channelARN =
+  //     'arn:aws:kinesisvideo:us-west-2:123456789012:channel/test-channel/1234567890';
+
+  //   const kinesisVideo = new AWS.KinesisVideo({
+  //     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //     region: 'us-west-2',
+  //     correctClockSkew: true,
+  // })
+}
 }
